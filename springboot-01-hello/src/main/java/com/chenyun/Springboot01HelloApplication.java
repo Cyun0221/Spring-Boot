3342519cf8759e6ff09c20850d001eaca7c9615a
陈云 <36301098+chenger9@users.noreply.github.com>
2019-09-12 15:42:25 +0800

    Add files via upload

---
+++ springboot-01-hello/src/main/java/com/chenyun/Springboot01HelloApplication.java
@@ -0,0 +1,12 @@
+package com.chenyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01HelloApplication.class, args);
+    }
+}
